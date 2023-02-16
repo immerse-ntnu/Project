91789e354bb13a8eef7a68b0255e3c03287da2d9
David Omanovic <davidomanovic@gmail.com>
2023-02-16 20:00:20 +0100

    UI updates
    Char selection, settings, main menu

---
+++ Assets/Scripts/UI/GameLoader.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameLoader : MonoBehaviour
+{
+    
+    public void LoadMainMenu() // Load main menu buddy
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void LoadSelection() // Load scene for selection menu
+    {
+        SceneManager.LoadScene("Selection");
+    }
+    
+    public void LoadSettings() // Load scene for settings menu
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    
+    public void LoadCreation() // Load scene for character creation menu
+    {
+        SceneManager.LoadScene("CharacterCreation");
+    }
+}
---
+++ Assets/Scripts/UI/Settings.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+}
